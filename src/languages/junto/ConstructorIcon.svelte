0194cc0233ae127efe7bd4d4d933806486f6738d
jdeepee <joshuadparkin@gmail.com>
2020-10-10 16:49:09 +0100

    init junto lang

---
+++ src/languages/junto/ConstructorIcon.svelte
@@ -0,0 +1,27 @@
+<svelte:options tag={null}/>
+
+<script lang="ts">
+    export let commitExpression
+
+    let text = ""
+
+</script>
+
+<div class="container">
+    <input bind:value={text}>
+    <button on:click={()=>commitExpression(text)}>Commit</button>
+</div>
+
+
+<style>
+    .container {
+        color: burlywood;
+        width: 400px;
+        height: 300px;
+    }
+
+    input {
+        width: 100%;
+        height: 200px;
+    }
+</style>
